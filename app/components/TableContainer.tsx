1916d64c823b93ba67c331979454c7351c2d4942
Ahmed Abas Al-Ali <bfhetyrhyr12@gmail.com>
2025-06-21 16:47:25 +0300

    init

--- app/components/TableContainer.tsx
+++ app/components/TableContainer.tsx
@@ -52,7 +52,7 @@ const TableContainer = ({
         params.sortDirection = sortDirection.sortDirection;
       }
 
-      const response = await axios.get(`${process.env.NEXT_PUBLIC_API_BASE_URL}${apiUrl}`, {
+      const response = await axios.get(`http://209.127.228.55:8082/api/carcontracts/v1${apiUrl}`, {
         params,
         headers: {
           'Content-Type': 'application/json',
